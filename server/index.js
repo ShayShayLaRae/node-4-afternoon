026d12b9663ad5dbe52df5c4f3c054dd50aab993
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-04 14:42:50 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const checkForSession = require('./middlewares/checkForSession');
+const swagController = require('./controllers/swagController');
+
+const app= express();
+
+let {SERVER_PORT, SESSION_SECRET} = process.env;
+
+app.use(express.json());
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true
+    })
+);
+app.use(checkForSession);
+
+//Endpoints
+app.get('/api/swag', swagController.read);
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Whatever, something clever ${SERVER_PORT}`);
+    
+});
