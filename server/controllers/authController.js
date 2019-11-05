9b1d2348356b6da2f1be2cd57b356fc7a4b6b9fd
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-05 15:42:56 -0700

    progress

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,2 +1,26 @@
-const users = require('./models/users');
+const users = require('../models/users');
 
+let id = 1;
+
+module.exports = {
+    register: (req, res) => {
+        const {session} = req;
+        const {username, password} = req.body;
+        
+        users.push({id, username, password});
+        id++;
+
+        session.user.username = username;
+
+        res.status(200).send(session.user);
+    },
+    login: (req, res) => {
+
+    },
+    signout: (req, res) => {
+
+    },
+    getUser: (req, res) => {
+
+    }
+}
