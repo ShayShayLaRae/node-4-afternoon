b190b687502b27a31e8aac63e2301ec2f5fc0c79
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-05 20:10:37 -0700

    ugh

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,12 +15,24 @@ module.exports = {
         res.status(200).send(session.user);
     },
     login: (req, res) => {
+        const {session} = req;
+        const {username, password} = req.body;
 
+        const user = users.find(user.username === username && user.password === password);
+
+        if (user) {
+            session.user.username = user.username;
+            res.status(200).send(session.user);
+        } else {
+            res.status(500).send('Unauthorized');
+        }
     },
     signout: (req, res) => {
-
+        req.session.destroy();
+        res.status(200).send(req.session);
     },
     getUser: (req, res) => {
-
+        const {session} = reg;
+        res.status(200).send(session.user);
     }
 }
--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const checkForSession = require('./middlewares/checkForSession');
 const swagController = require('./controllers/swagController');
+const authController = require('./controllers/authController');
+const cartController = require('./controllers/cartController');
+const searchController = require('./controllers/searchController');
 
 const app= express();
 
 let {SERVER_PORT, SESSION_SECRET} = process.env;
 
+//Middleware
 app.use(express.json());
 app.use(
     session({
@@ -17,9 +21,26 @@ app.use(
     })
 );
 app.use(checkForSession);
+app.use(express.static(`${__dirname}/../build`));
 
 //Endpoints
+
+//Auth
+app.post('/api/register', authController.register);
+app.post('/api/login', authController.login);
+app.post('/api/signout', authController.signout);
+app.get('/api/user', authController.getUser);
+
+//Swag
 app.get('/api/swag', swagController.read);
+
+//Cart
+app.post('/api/cart/checkout', cartController.checkout);
+app.post('/api/cart/:id', cartController.add);
+app.delete('/api/cart/:id', cartController.delete);
+
+//Search
+app.get('/api/search', searchController.search);
 
 app.listen(SERVER_PORT, () => {
     console.log(`Whatever, something clever ${SERVER_PORT}`);
